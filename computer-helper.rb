43e572506c1ca0c1aaf9e169eeb70fcfdb16b7d6
Chanyoung Kim <chanyoung.j.kim@gmail.com>
2015-09-25 12:24:49 -0400

    Remove unnecessary method

---
+++ computer-helper.rb
@@ -0,0 +1,23 @@
+require 'rubygems'
+require 'geokit'
+require'pry'
+
+Geokit::default_units = :miles
+Geokit::default_formula = :sphere
+Geokit::Geocoders::request_timeout = 3
+
+class Location
+  # include Geokit::Mappable
+
+  attr_reader :current_loc
+
+  def initialize(address = '48 Wall St, New York, NY')
+    @current_loc = Geokit::Geocoders::GoogleGeocoder.geocode(address).ll.split(',')
+  end
+
+  def calculate_distance_to(lat, long)
+    here = Geokit::LatLng.new(*current_loc)
+    there = Geokit::LatLng.new(lat, long)
+    here.distance_to(there)
+  end
+end
