1e167dd2ff0424255e1b0cb6e8232547a146450c
Chanyoung Kim <chanyoung.j.kim@gmail.com>
2015-09-25 14:26:51 -0400

    Add selective parser

--- computer.rb
+++ computer.rb
@@ -3,10 +3,15 @@ require 'open-uri'
 
 module Computer
 
-  def self.parser
-  file = open("https://data.cityofnewyork.us/resource/erm2-nwe9.json")
-  data = JSON.parse(file.read)
+  def self.parser()
+    file = open("https://data.cityofnewyork.us/resource/erm2-nwe9.json")
+    data = JSON.parse(file.read)
   end
 
+  def self.selective_parser(type)
+    type = type.split(" ").join("+")
+    file = open("https://data.cityofnewyork.us/resource/erm2-nwe9?Complaint+Type=#{type}")
+    data = JSON.parse(file.read)
+  end
 end
 
